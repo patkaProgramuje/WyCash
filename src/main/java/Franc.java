232e7e9f466876c4c1b1f165a7c744a7bcc092bd
patka <patrycja.hyjek@gmail.com>
2019-11-25 16:59:47 +0100

    Add Franc class.

---
+++ src/main/java/Franc.java
@@ -0,0 +1,18 @@
+class Franc {
+
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Franc dollar = (Franc) o;
+        return amount == dollar.amount;
+    }
+}
