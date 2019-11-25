3cb0103c2e4a0834ed112cf0ad36e06bc41ce247
patka <patrycja.hyjek@gmail.com>
2019-11-25 16:55:55 +0100

    Add equals method to Dollar class and test in DollarTest.

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,12 +1,18 @@
-public class Dollar {
+class Dollar {
 
     int amount;
 
-    Dollar(int amount){
+    Dollar(int amount) {
         this.amount = amount;
     }
 
-    void times(int multiplier){
-        amount *= multiplier;
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
     }
 }
