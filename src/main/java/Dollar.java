f7bb08963fa410feb4af4d19987236ea068d83e3
patka <patrycja.hyjek@gmail.com>
2019-11-25 16:23:21 +0100

    Correct class Dollar.

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -1,4 +1,12 @@
 public class Dollar {
 
+    int amount;
 
+    Dollar(int amount){
+        this.amount = amount;
+    }
+
+    void times(int multiplier){
+        amount *= multiplier;
+    }
 }
