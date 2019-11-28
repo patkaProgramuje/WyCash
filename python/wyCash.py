7ed98641c7a23f21d376cb943d079fbcdae3eeb2
patka <patrycja.hyjek@gmail.com>
2019-11-28 14:06:22 +0100

    Wrote new tests casees, changed the interface of the run method.

--- python/wyCash.py
+++ python/wyCash.py
@@ -2,12 +2,20 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
-    def run(self):
+    def run(self, result):
+        result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except:
+            result.testFailed()
+        self.tearDown()
 
     def setUp(self):
+        pass
+
+    def tearDown(self):
         pass
 
 
@@ -15,31 +23,84 @@ class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self, name)
 
-    def testMethod(self):
-        self.wasRun = 1
-
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
+    def testBrokenMethod(self):
+        raise Exception
 
 
 class TestCaseTest(TestCase):
     def setUp(self):
-        self.test = WasRun("testMethod")
+        self.test = TestResult()
 
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run(result)
+        assert ("setUp testMethod tearDown " == test.log)
 
-    def testSetUp(self):
-        self.test.run()
-        assert self.test.wasSetUp
+    def testResult(self):
+        test = WasRun("testMethod")
+        test.run(result)
+        assert ("1 run, 0 failed" == result.summary())
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        test.run(result)
+        assert "1 run, 1 failed", result.summary()
+
+    def testFailedResultFormatting(self):
+        result.testStarted()
+        result.testFailed()
+        assert "1 run, 1 failed" == result.summary()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(result)
+        assert "2 run, 1 failed" == result.summary()
 
 
-TestCaseTest("testSetUp").run()
-TestCaseTest("testRunning").run()
+class TestSuite:
+    def __init__(self):
+        self.tests = []
 
-test = WasRun("testMethod")
-print(test.wasRun)
-test.run()
-print(test.wasRun)
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+        self.errorCount = 0
+
+    def testFailed(self):
+        self.errorCount = self.errorCount + 1
+
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+
+    def summary(self):
+        return "%d run, %d failed" % (self.runCount, self.errorCount)
+
+
+suite = TestSuite()
+print(TestCaseTest("testTemplateMethod"))
+print(TestCaseTest("testResult"))
+print(TestCaseTest("testFailedResultFormatting"))
+print(TestCaseTest("testFailedResult"))
+print(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
